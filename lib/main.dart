7af51833da0e22223ce145c61c3f63cd5af3b3fe
Junior Medeiros <juniormedeirosluce@gmail.com>
2022-11-17 17:18:39 -0300

    Solicitação de rede para teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+  Future<http.Response> fetchAlbum() {
+    return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+  }
+  
   const MyApp({super.key});
 
   @override
